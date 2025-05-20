5cb5aadb2067d3f293e885494a1175812f01a3b0
ymid0610 <ymid0610@gmail.com>
2025-05-21 00:07:54 +0900

    몬스터 헤더, 몬스터 cpp 수정

--- keyvsMouse/keyvsMouse/MONSTER.cpp
+++ keyvsMouse/keyvsMouse/MONSTER.cpp
@@ -1,3 +1,70 @@
 #include "MONSTER.h"
-#include <stdlib.h>
 
+
+MONSTER::MONSTER(int nhp, int npower, int nx, int ny, int nMspeed, int nAspeed, int nrange, int nsize, int nDropItem)
+{
+}
+
+MONSTER::~MONSTER()
+{
+}
+
+void MONSTER::SetSpot(int nx, int ny)
+{
+}
+
+void MONSTER::SetHp(int nhp)
+{
+}
+
+void MONSTER::SetPower(int npower)
+{
+}
+
+void MONSTER::SetMspeed(int nMspeed)
+{
+}
+
+void MONSTER::SetAspeed(int nAspeed)
+{
+}
+
+void MONSTER::SetSize(int nsize)
+{
+}
+
+void MONSTER::SetDropItem(int nDropItem)
+{
+}
+
+void MONSTER::SetView(int nview)
+{
+}
+
+void MONSTER::SetRect(RECT nrect)
+{
+}
+
+void MONSTER::MoveToPlayer(POINT player1)
+{
+}
+
+void MONSTER::MoveToMachine(POINT buliding)
+{
+}
+
+void MONSTER::Hit(int power)
+{
+}
+
+void MONSTER::Death()
+{
+}
+
+void MONSTER::Update()
+{
+}
+
+void MONSTER::Draw(HDC hDC)
+{
+}
--- keyvsMouse/keyvsMouse/MONSTER.h
+++ keyvsMouse/keyvsMouse/MONSTER.h
@@ -1,16 +1,17 @@
 #pragma once
 #include <windows.h>
+#include <stdlib.h>
 
 enum DIRECTION {
 	left = 0, right = 1, up = 2, down = 3
 };
 
-
 class MONSTER
 {
 private:
-	int hp;
 	int x, y; // 좌표
+	int hp; // 체력
+	int power; // 공격력
 	int Mspeed; // 이동속도
 	int Aspeed; // 공격속도
 	int range; // 공격 사거리
@@ -22,31 +23,31 @@ private:
 	RECT rect; // 그리기용 좌표
 
 public:
-	MONSTER(int x, int y, int Mspeed, int Aspeed, int range, int size, int Act, int view, RECT rect); // 생성자
+	MONSTER(int nhp, int npower, int nx, int ny, int nMspeed, int nAspeed, int nrange, int nsize, int nDropItem); // 생성자
+	
 	~MONSTER(); // 소멸자
 
 	// 값 변경 함수
-	void SetSpot(int x, int y) {}; // 좌표 설정
-	void SetMspeed(int Mspeed) {}; // 이동속도 설정
-	void SetAspeed(int Aspeed) {}; // 공격속도 설정
-	void SetSize(int size) {}; // 몬스터 크기 설정
-	void SetAct(int Act) {}; // 드랍되는 아이템 시드값 설정
-	void SetView(int view) {}; // 몬스터 시선 설정
-	void SetRect(RECT rect) {}; // 그리기용 좌표
-
-	// 업데이트 함수
-	void Update();
+	void SetSpot(int nx, int ny); // 좌표 설정
+	void SetHp(int nhp); // 체력 설정
+	void SetPower(int npower); // 공격력 설정
+	void SetMspeed(int nMspeed); // 이동속도 설정
+	void SetAspeed(int nAspeed); // 공격속도 설정
+	void SetSize(int nsize); // 몬스터 크기 설정
+	void SetDropItem(int nDropItem); // 드랍되는 아이템 시드값 설정
+	void SetView(int nview); // 몬스터 시선 설정
+	void SetRect(RECT nrect); // 그리기용 좌표
 
 	// 이동함수
-	void MoveToPlayer(int x, int y, int Mspeed, int view, POINT player1) {}; // 플레이어 위치 이동 함수
-	void MoveToMachine(int x, int y, int Mspeed, int view, POINT buliding) {}; // 기물 위치 근처 이동 함수
+	void MoveToPlayer(POINT player1); // 플레이어 위치 이동 함수
+	void MoveToMachine(POINT buliding); // 기물 위치 근처 이동 함수
 
 	// 행동함수
-	void Hit() {}; // 피격 함수
-	void Death(int x, int y, int DropItem) {}; // 죽음 함수
+	void Hit(int power); // 피격 함수
+	void Death(); // 죽음 함수
 
 	// 부가요소 함수
-	void Update(float deltaTime) {}; // 위치 업데이트
-	void Draw(HDC hDC) {}; // 그리기
+	void Update(); // 위치 업데이트
+	void Draw(HDC hDC); // 그리기
 };
 
